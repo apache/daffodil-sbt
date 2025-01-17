08164d820fac72aea04e48bb2974ebe0f13c5e7e
Steve Lawrence <slawrence@apache.org>
2025-01-17 12:09:53 -0500

    Add support for daffodil-tdml-junit library
    This adds a dependency to daffodil-tdml-junit so that DFDL schema
    projects can now make use of the much more compact TDML Runner API when
    using JUnit for testing.
    
    Note that this adds the dependency for all Daffodil versions 3.2.0 or
    newer, so that even older DFDL schema projects can use the more compact
    syntax. This is done by making it an intranstive dependency for older
    versions.
    
    Closes #84

---
+++ src/sbt-test/sbt-daffodil/tdml-junit-01/src/test/scala/com/example/test.scala
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example
+
+import org.apache.daffodil.junit.tdml.TdmlSuite
+import org.apache.daffodil.junit.tdml.TdmlTests
+
+import org.junit.Test
+
+object TestExample extends TdmlSuite {
+  val tdmlResource = "/com/example/test.tdml"
+}
+
+class TestExample extends TdmlTests {
+  val tdmlSuite = TestExample
+
+  @Test def test01 = test
+}
