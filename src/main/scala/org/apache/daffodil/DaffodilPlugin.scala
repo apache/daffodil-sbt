7906d043deb4230e2c9e82209f766ad0fc5d7980
Josh Adams <jadams@owlcyberdefense.com>
2025-06-03 13:55:47 -0400

    Change default version of Daffodil to 3.11.0

--- src/main/scala/org/apache/daffodil/DaffodilPlugin.scala
+++ src/main/scala/org/apache/daffodil/DaffodilPlugin.scala
@@ -220,7 +220,7 @@ object DaffodilPlugin extends AutoPlugin {
     /**
      * Default Daffodil version
      */
-    daffodilVersion := "3.10.0",
+    daffodilVersion := "3.11.0",
 
     /**
      * Disable uncommon features by default, schema projects must explicitly enable them to use
