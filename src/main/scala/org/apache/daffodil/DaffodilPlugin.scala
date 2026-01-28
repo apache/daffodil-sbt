c8da09bcacc6be5f447ed113eb3893c3d699fb66
Steve Lawrence <slawrence@apache.org>
2026-01-28 11:03:39 -0500

    Prepare for 1.6.0 release
    Bump default daffodilVersion to 4.1.0

--- src/main/scala/org/apache/daffodil/DaffodilPlugin.scala
+++ src/main/scala/org/apache/daffodil/DaffodilPlugin.scala
@@ -229,7 +229,7 @@ object DaffodilPlugin extends AutoPlugin {
     /**
      * Default Daffodil version
      */
-    daffodilVersion := "4.0.0",
+    daffodilVersion := "4.1.0",
 
     /**
      * Disable uncommon features by default, schema projects must explicitly enable them to use
